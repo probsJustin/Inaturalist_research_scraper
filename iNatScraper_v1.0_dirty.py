065195d413f37657d59d9b3fb1237f2ae3618e9a
Justin Hagerty <wheelsmanx@gmail.com>
2022-09-14 11:10:18 -0400

    page testing

--- iNatScraper_v1.0_dirty.py
+++ iNatScraper_v1.0_dirty.py
@@ -34,6 +34,19 @@ def get_all_pages_as_dict(number_of_pages):
     return dict_to_return
 
 internal_logger.debug(f'Testing.....')
-response = get_identifications(taxon_id=[52818], per_page=200, page=10)
+response = get_identifications(taxon_id=[52818], per_page=200, page=1)
 number_of_pages_needed = response_handler.get_total_pages(response, 200)
-print(number_of_pages_needed)
+
+pagedResponse = dict()
+for x in range(1, number_of_pages_needed):
+    print(f'page: {x}')
+    try:
+        pagedResponse[x] = get_identifications(taxon_id=[52818], per_page=200, page=x)
+    except Exception as error:
+        print(error)
+        break
+
+for y in pagedResponse:
+    print(f'\nPAGE: {y}\n')
+    pprint(pagedResponse[y])
+
